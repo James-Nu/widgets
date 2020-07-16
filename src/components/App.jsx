fc396cc74e1be4b18315e09e1731dc0cd8a34ab2
James Nunoo <jamesnunoo@Jamess-MacBook-Pro.local>
2020-07-16 11:27:30 +0300

    init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import Accordion from "./Accordion";
+import Search from "./Search";
+import Dropdown from "./Dropdown";
+import Translate from "./Translate";
+import Route from "./Route";
+import Header from "./Header";
+
+const items = [
+  {
+    title: "What is React",
+    content: "React is a frontend javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS librabry among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The color Red",
+    value: "Red",
+  },
+  {
+    label: "The color Green",
+    value: "green",
+  },
+  {
+    label: "The color Blue",
+    value: "blue",
+  },
+];
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div>
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+
+      <Route path="/list">
+        <Search />
+      </Route>
+
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
+};
+
+export default App;
